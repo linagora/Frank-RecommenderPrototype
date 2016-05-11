32bbfa370413c360239555b3d827326f81f162c6
Mignastor <frank@macbook-air-de-code.laas.fr>
2016-05-11 15:27:51 +0200

    t3

--- src/main/scala/EnronMatrixCreation.scala
+++ src/main/scala/EnronMatrixCreation.scala
@@ -28,9 +28,10 @@ object EnronMatrixCreation extends App{
   //create one row between each mail dent by the user
   val matrix : ArrayList[Array[Int]] = new ArrayList[Array[Int]]
   var index = 0
-  val userReceivedMail:Array[EnronRow] =  EnronReceivedMailRDD.collect().toMap.get(25).get.toArray
+  val userReceivedMail : Array[EnronRow] =  EnronReceivedMailRDD.collect().toMap.get(25).get.toArray
   val row: Array[Int] = Array.fill[Int](185)(0)
-  EnronSentMailRDD.collect().toMap.get(25).get.foreach(sentMail => {
+
+  EnronSentMailRDD.collect().toMap.get(25).get.foreach( sentMail => {
     val sentMailTime = sentMail._1
     while ( sentMailTime > userReceivedMail(index)._1 ) {
       row(userReceivedMail(index)._2)+=1
@@ -42,9 +43,4 @@ object EnronMatrixCreation extends App{
   })
 
   println("\n Taille de la matrice " + matrix.size() + "\n")
-
-
-
-  // for each mail sent, the row represents all current inbox mails that are waiting for and answer
-  // we have a surpervised problem: input is mails in box waiting for answer and output is to whom the answer has to be sent first
 }
