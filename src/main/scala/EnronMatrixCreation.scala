acf7755e62bbd88ca2b95669388085c810d1f021
Mignastor <frank@macbook-air-de-code.laas.fr>
2016-05-20 11:21:34 +0200

    full matrix creation to hfs

--- src/main/scala/EnronMatrixCreation.scala
+++ src/main/scala/EnronMatrixCreation.scala
@@ -23,47 +23,56 @@ object EnronMatrixCreation extends App{
       (lineArray(0).toInt,lineArray(1).toInt,lineArray(2).toInt)
     })
 
+  //create one row between each mail dent by the user
+  val matrix: ListBuffer[Array[Int]] = new ListBuffer[Array[Int]]()
+
   // Mails sent TO user
   val EnronReceivedMailRDD: RDD[(Int,Iterable[EnronRow])] = EnronRDD.groupBy(_._3)
-  //mails sent To user 25
-  val userReceivedMail=  EnronReceivedMailRDD.collect().filter(_._1==25).head._2.toArray
-
   //mails SENT BY (FROM) user
-  val EnronSentMailRDD : RDD[(Int,Iterable[EnronRow])]= EnronRDD.groupBy(_._2)
-  // mails SENT By (FROM) user 25
-  val userSentMails: Array[EnronRow] = EnronSentMailRDD.collect().filter(_._1==25).head._2.toArray
+  val EnronSentMailRDD: RDD[(Int, Iterable[EnronRow])] = EnronRDD.groupBy(_._2)
+
+  val nbUsers = EnronReceivedMailRDD.collect().toMap.keys.toArray
+  for ( i <- nbUsers) {
+    //mails sent To user i
+    val userReceivedMail = EnronReceivedMailRDD.collect().filter(_._1 == i).head._2.toArray
 
 
-  //create one row between each mail dent by the user
-  val matrix : ListBuffer[Array[Int]] = new ListBuffer[Array[Int]]()
-  var index = 0
+    // mails SENT By (FROM) user i
+    val userSentMails: Array[EnronRow] = EnronSentMailRDD.collect().filter(_._1 == i).head._2.toArray
 
 
-  val row: Array[Int] = Array.fill[Int](185)(0)
+    var index = 0
 
-    for (receivedMail <- userReceivedMail ){
+
+    val row: Array[Int] = Array.fill[Int](185)(0)
+
+    for (receivedMail <- userReceivedMail) {
       //userSentMails.foreach( sentMail => {
 
-    val receivedmailTime = receivedMail._1
-    if (index <= userReceivedMail.size ) {
-      if (receivedmailTime < userSentMails(index)._1) {
-        row(userReceivedMail(index)._2) += 1
-        index += 1
-      }
-      else {
-        row(userSentMails(index)._2) -= 1
-        row(184) = userSentMails(index)._3
-        matrix.append(row)
+      val receivedmailTime = receivedMail._1
+      if (index <= userReceivedMail.size) {
+        if (receivedmailTime < userSentMails(index)._1) {
+          row(userReceivedMail(index)._2) += 1
+          index += 1
+        }
+        else {
+          row(userSentMails(index)._2) -= 1
+          row(184) = userSentMails(index)._3
+          matrix.append(row)
+        }
       }
     }
+
+    //println("\n Taille de userRceivedMail 25 : " + userReceivedMail.size + "\n")
+    //println("\n Taille de toto : " + userSentMails.size + "\n")
+
+
   }
+  //val matrixString=matrix.map(_.mkString(" , "))
+  //println("\n Taille de la matrice " + matrix.length + "\n")
+  //println("\n Matrix :\n "+ matrixString.mkString("\n"))
 
-  val matrixString=matrix.map(_.mkString(" , "))
-
-  println("\n Taille de la matrice " + matrix.length + "\n")
-  println("\n Taille de userRceivedMail 25 : " + userReceivedMail.size + "\n")
-  println("\n Taille de toto : " + userSentMails.size + "\n")
-  println("\n Matrix :\n "+ matrixString.mkString("\n"))
+  sc.parallelize(matrix).saveAsTextFile("hdfs://master.spark.com/Enron/MatrixResult")
 
 
 }
