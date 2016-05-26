1bbc42c994102e788674e53e276b508ca05a300a
Mignastor <frank@franks-macbook-pro.home>
2016-05-26 12:51:19 +0200

    Hdfs other user

--- src/main/scala/EnronMatrixCreation.scala
+++ src/main/scala/EnronMatrixCreation.scala
@@ -35,13 +35,13 @@ object EnronMatrixCreation extends App{
 
   //for ( i <- nbUsers) {
     //mails sent To user i
-    val userReceivedMail = EnronReceivedMailRDD.collect().filter(_._1 == 25).head._2.toArray
+    val userReceivedMail = EnronReceivedMailRDD.collect().filter(_._1 == 183).head._2.toArray
     // mails SENT By (FROM) user i
-    val userSentMails: Array[EnronRow] = EnronSentMailRDD.collect().filter(_._1 == 25).head._2.toArray
+    val userSentMails: Array[EnronRow] = EnronSentMailRDD.collect().filter(_._1 == 183).head._2.toArray
 
     var index = 0
 
-    val row: Array[Int] = Array.fill[Int](185)(0)
+    val row: Array[Int] = Array.fill[Int](184)(0)
 
     for (receivedMail <- userReceivedMail) {
       //userSentMails.foreach( sentMail => {
