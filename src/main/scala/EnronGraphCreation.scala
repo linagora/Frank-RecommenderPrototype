6e44899ba7a27984ff87f1ba34a15e6961b4261a
Mignastor <frank@macbook-air-de-code.laas.fr>
2016-06-02 16:11:21 +0200

    test graph with card

--- src/main/scala/EnronGraphCreation.scala
+++ src/main/scala/EnronGraphCreation.scala
@@ -20,30 +20,34 @@ object EnronGraphCreation extends App{
     )
 
   val sentMails = sc.wholeTextFiles("hdfs://master.spark.com/Enron/maildir/*/_sent_mail/*").map(_._2)
-val nbUsers = new ListBuffer[String]
+val nbUsers = new ListBuffer[Int]
   val tripleRDD = sentMails.collect().flatMap(mail=>{
     val toLine = mail.split("\n").filter(line=> line.contains("To: "))
     val ccLine = mail.split("\n").filter(line=> line.contains("cc: "))
     val fromLine = mail.split("\n").filter(line=> line.contains("From: "))
     val from = fromLine.head.split(" ")(1).hashCode
-    val toArray = toLine.head.split("To: ")(1).split(",")
-    val ccArray = ccLine.head.split("cc: ")(1).split(",")
+    val toArray = toLine.head.split("To: ")(1).split(",").map(_.hashCode)
+    val ccArray = ccLine.head.split("cc: ")(1).split(",").map(_.hashCode)
 
-    val listEdges = new ListBuffer[(Int,String,String)]
+    val listEdges = new ListBuffer[(Int,Int,String)]
     for (to <- toArray){
       listEdges.append((from,to,"to"))
     }
     for (cc <- ccArray){
       listEdges.append((from,cc,"cc"))
     }
-  if (!nbUsers.contains(fromLine.head.split(" ")(1))){
-    nbUsers.append(fromLine.head.split(" ")(1))
+  if (!nbUsers.contains(from)){
+    nbUsers.append(from)
   }
 
   listEdges.toList
   })
 
-  val edgesRDD = sc.parallelize(tripleRDD).map(triple => Edge(triple._1,triple._2.hashCode,triple._3))
+  // Replace arc string by count
+  val triplesArcCountRDD:RDD[(Int,Int,Int)] = sc.parallelize(tripleRDD).map(triple =>(triple._1+""+triple._2,(triple._1,triple._2,1))).reduceByKey((triple1,triple2)=>(triple1._1,triple1._2,triple1._3+triple2._3)).map(_._2)
+
+  //Create Triples Edges
+  val edgesRDD = triplesArcCountRDD.map(triple => Edge(triple._1,triple._2.hashCode,triple._3))
 
   // Create the Graph
   val graph = Graph.fromEdges(edgesRDD, "defaultProperty")
