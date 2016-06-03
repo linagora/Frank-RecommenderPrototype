9068df57be73a8f71427978a4b52fa6b3e4a3211
Mignastor <frank@franks-macbook-pro.home>
2016-06-03 18:33:05 +0200

    println new

--- src/main/scala/EnronGraphCreation.scala
+++ src/main/scala/EnronGraphCreation.scala
@@ -22,7 +22,9 @@ object EnronGraphCreation extends App{
   )
 
   val sentMails = sc.wholeTextFiles("hdfs://master.spark.com/Enron/maildir/*/_sent_mail/*").map(_._2)
-  val nbUsers = new ListBuffer[String]
+  val fromUsers = new ListBuffer[String]
+  val toUsers = new ListBuffer[String]
+  val ccUsers = new ListBuffer[String]
 
   // RFC Standard
   val mailPattern = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])".r
@@ -32,17 +34,22 @@ object EnronGraphCreation extends App{
     val ccLine = mail.split("\n").filter(line=> line.contains("cc: ")).head
     val fromLine = mail.split("\n").filter(line=> line.contains("From: ")).head
     val from : String = (mailPattern findFirstIn fromLine).get
-    val toArray : Array[Int] = (mailPattern findAllIn toLine).toArray.map(_.hashCode)
-    val ccArray : Array[Int] = (mailPattern findAllIn ccLine).toArray.map(_.hashCode)
-
+    val toArray : Array[String] = (mailPattern findAllIn toLine).toArray.map(toUser=>{
+      toUsers.append(toUser)
+      toUser
+    })
+    val ccArray : Array[String] = (mailPattern findAllIn ccLine).toArray.map(ccUser=>{
+      ccUsers.append(ccUser)
+      ccUser
+    })
     val listEdges = new ListBuffer[(Int,Int,String)]
-    if (!nbUsers.contains(from)) {
-      nbUsers.append(from)
+    if (!fromUsers.contains(from)) {
+      fromUsers.append(from)
       for (to <- toArray) {
-        listEdges.append((nbUsers.indexOf(from), to, "to"))
+        listEdges.append((fromUsers.indexOf(from), toUsers.indexOf(to), "to"))
       }
       for (cc <- ccArray) {
-        listEdges.append((nbUsers.indexOf(from), cc, "cc"))
+        listEdges.append((fromUsers.indexOf(from), ccUsers.indexOf(cc), "cc"))
       }
     }
 
@@ -67,11 +74,14 @@ object EnronGraphCreation extends App{
   println("\n il y a "+sentMails.count()+" mail envoyés \n")
   println("\nnum edges = " + graph.numEdges +"\n")
   println("\nnum vertices = " + graph.numVertices+"\n")
-  println("\nthere are "+ nbUsers.size + " users in this dataset\n")
+  //println("\nthere are "+ fromUsers.size + " users in this dataset\n")
   println("\nthere are : "+usersSentMails.count()+" users that sent emails\n")
   println("\nthere are : "+usersReceivedMails.count()+" users that received emails\n")
 
-  println(nbUsers.mkString("\n"))
+  //println(fromUsers.mkString("\n"))
+  println("\nfromUsers count :"+fromUsers.length)
+  println("toUsers count :"+toUsers.length)
+  println("ccUsers count :"+ccUsers.length)
 
 
 }
