1759b30268ddcfede820f67c29396ab14e18d9c6
Mignastor <frank@Franks-MBP.gso.lan>
2016-06-06 09:49:16 +0200

    one users array

--- src/main/scala/EnronGraphCreation.scala
+++ src/main/scala/EnronGraphCreation.scala
@@ -22,40 +22,40 @@ object EnronGraphCreation extends App{
   )
 
   val sentMails = sc.wholeTextFiles("hdfs://master.spark.com/Enron/maildir/*/_sent_mail/*").map(_._2)
-  val fromUsers = new ListBuffer[String]
-  val toUsers = new ListBuffer[String]
-  val ccUsers = new ListBuffer[String]
+  val users = new ListBuffer[String]
+
 
   // RFC Standard
   val mailPattern = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])".r
 
-  val tripleRDD = sentMails.collect().flatMap(mail=>{
-    val toLine = mail.split("\n").filter(line=> line.contains("To: ")).head
-    val ccLine = mail.split("\n").filter(line=> line.contains("cc: ")).head
-    val fromLine = mail.split("\n").filter(line=> line.contains("From: ")).head
-    val from : String = (mailPattern findFirstIn fromLine).get
-    val toArray : Array[String] = (mailPattern findAllIn toLine).toArray.map(toUser=>{
-      if (!toUsers.contains(toUser)) {
-        toUsers.append(toUser)
+  val tripleRDD = sentMails.collect().flatMap(f = mail => {
+    val toLine = mail.split("\n").filter(line => line.contains("^To: ".r)).head
+    val ccLine = mail.split("\n").filter(line => line.contains("^cc: ".r)).head
+    val fromLine = mail.split("\n").filter(line => line.contains("^From: ".r)).head
+    val from: String = (mailPattern findFirstIn fromLine).get
+    val toArray: Array[String] = (mailPattern findAllIn toLine).toArray.map(toUser => {
+      if (!users.contains(toUser)) {
+        users.append(toUser)
       }
       toUser
     })
-    val ccArray : Array[String] = (mailPattern findAllIn ccLine).toArray.map(ccUser=>{
-      if (!ccUsers.contains(ccUser)) {
-        ccUsers.append(ccUser)
+    val ccArray: Array[String] = (mailPattern findAllIn ccLine).toArray.map(ccUser => {
+      if (!users.contains(ccUser)) {
+        users.append(ccUser)
       }
       ccUser
     })
-    val listEdges = new ListBuffer[(Int,Int,String)]
-    if (!fromUsers.contains(from)) {
-      fromUsers.append(from)
-      for (to <- toArray) {
-        listEdges.append((fromUsers.indexOf(from), toUsers.indexOf(to), "to"))
-      }
-      for (cc <- ccArray) {
-        listEdges.append((fromUsers.indexOf(from), ccUsers.indexOf(cc), "cc"))
-      }
+    val listEdges = new ListBuffer[(Int, Int, String)]
+    if (!users.contains(from)) {
+      users.append(from)
     }
+    for (to <- toArray) {
+      listEdges.append((users.indexOf(from), users.indexOf(to), "to"))
+    }
+    for (cc <- ccArray) {
+      listEdges.append((users.indexOf(from), users.indexOf(cc), "cc"))
+    }
+
 
     listEdges.toList
   })
@@ -86,9 +86,8 @@ val receivedmailcount=usersReceivedMails.count()
   println("\nthere are : "+receivedmailcount+" users that received emails\n")
 
   //println(fromUsers.mkString("\n"))
-  println("\nfromUsers count :"+fromUsers.length)
-  println("toUsers count :"+toUsers.length)
-  println("ccUsers count :"+ccUsers.length)
+  println("\nUsers count :"+users.length+"\n")
+
 
 
 }
