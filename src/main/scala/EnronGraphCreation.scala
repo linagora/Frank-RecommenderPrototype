af3d186cfe115925ed7f8e11c25becee80952ae6
Mignastor <frank@macbook-air-de-code.laas.fr>
2016-06-10 11:46:55 +0200

    save triplets to Hdfs

--- src/main/scala/EnronGraphCreation.scala
+++ src/main/scala/EnronGraphCreation.scala
@@ -122,6 +122,7 @@ object EnronGraphCreation extends App{
     .get(dest)
 
   val triplet = graph.triplets.collect()(25).toString()
+  graph.triplets.saveAsTextFile("hdfs://master.spark.com/Enron/GraphTriplets")
   //printings
 
   println("\n Shortest path between "+currentUser+ " and "+dest+" is "+shortestPath.toString)
