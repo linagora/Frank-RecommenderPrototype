cf833d31b0b03868da2ccf393072697c611aec92
Mignastor <frank@macbook-air-de-code.laas.fr>
2016-06-16 14:47:52 +0200

    comment saveastextfile

--- src/main/scala/EnronGraphCreation.scala
+++ src/main/scala/EnronGraphCreation.scala
@@ -30,6 +30,7 @@ object EnronGraphCreation extends App{
 
   // RFC Standard
   val mailPattern = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])".r
+  
   val listEdges = new ListBuffer[(Int, Int, String)]
 
   sentMails.collect().foreach(f = mail => {
@@ -112,8 +113,8 @@ object EnronGraphCreation extends App{
   val usersSentMails      : VertexRDD[Array[VertexId]] = graph.collectNeighborIds(EdgeDirection.Out)
 
   //graph.edges.saveAsTextFile("hdfs://master.spark.com/Enron/GraphEdges")
-  graph.vertices.saveAsTextFile("hdfs://master.spark.com/Enron/GraphVertices")
-  graph.triplets.saveAsTextFile("hdfs://master.spark.com/Enron/GraphTriplets")
+  //graph.vertices.saveAsTextFile("hdfs://master.spark.com/Enron/GraphVertices")
+  //graph.triplets.saveAsTextFile("hdfs://master.spark.com/Enron/GraphTriplets")
 
   // sender = 0 dest = 2 direct mail
   // 4 to 10006
