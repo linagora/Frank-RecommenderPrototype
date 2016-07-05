a151b3af56f4512aee9fe298cdd060958abd6134
Mignastor <frank@franks-macbook-pro.home>
2016-07-05 13:03:03 +0200

    50-50

--- src/main/scala/EnronTriangleGraph.scala
+++ src/main/scala/EnronTriangleGraph.scala
@@ -17,7 +17,7 @@ object EnronTriangleGraph extends App {
   )
 
   val mailDataset = sc.wholeTextFiles("hdfs://master.spark.com/Enron/maildir/*/_sent_mail/*").map(_._2)
-  val Array(sentMails,testSet) = mailDataset.randomSplit(Array(0.9,0.1))
+  val Array(sentMails,testSet) = mailDataset.randomSplit(Array(0.5,0.5))
   val users = new ListBuffer[String]
   val fromUsers = new ListBuffer[String]
   val anonymousGroup = new ListBuffer[String]
